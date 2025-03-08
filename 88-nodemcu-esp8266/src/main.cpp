bf8a1d844c372283150e83f3b864e6d0b81be74c
starshipmonkey <alaric.willi@gmail.com>
2025-03-08 16:40:13 +1000

    Clean up and roll back to a simpler base.

--- 88-nodemcu-esp8266/src/main.cpp
+++ 88-nodemcu-esp8266/src/main.cpp
@@ -1,31 +1,9 @@
-#include <Arduino.h>
-#include "States.h" // Include the header file for States.cpp
 
-// Function declarations
-void servoSetup();
-void servoSetUpper(int angle1);
-
-// Create an instance of the state machine
-StateMachine sm;
 
 void setup()
 {
-    Serial.begin(9600); // Initialize serial communication
-    servoSetup();
-    sm.update(); // Initial state update
 }
 
 void loop()
 {
-    servoSetUpper(90);
-    delay(1000);
-
-    // Example of state transitions
-    sm.transitionTo(State::Engaging);
-    sm.update();
-    delay(1000);
-
-    sm.transitionTo(State::Waiting);
-    sm.update();
-    delay(1000);
 }
